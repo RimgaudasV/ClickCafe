8aca6a3d80cc3a32634d35abf4fb6fda94e673bc
RimgaudasV <127286413+RimgaudasV@users.noreply.github.com>
2025-04-11 09:31:30 +0300

    Add project files.

---
+++ ClickCafé/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ClickCafé.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
